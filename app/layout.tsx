c4ab64e286a18ee36f39517a50b7e5d41193b506
Eric Nichols <ebn646@hotmail.com>
2025-02-10 15:37:19 -0500

    feat: added artist layout and sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,8 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="flex-1 w-full flex flex-col">
+              <nav className="w-full flex border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
@@ -53,7 +53,7 @@ export default async function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
                 <p>
                   Powered by{" "}
                   <a
