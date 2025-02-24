9b590a4543ced524a8245794b8e7a68c20fb88c1
Eric Nichols <ebn646@hotmail.com>
2025-02-24 09:15:50 -0500

    feat: added supabase suth folder

---
+++ lib/supabase/client.ts
@@ -0,0 +1,18 @@
+// Import the helper function to create a Supabase client
+// that is pre-configured for client-side (browser) usage.
+import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from '@supabase/supabase-js';
+
+// Create the browser client
+export const createBrowserSupabase = () =>
+  createBrowserClient(
+    // The Supabase URL is read from the environment variable and is used as the 
+    // Supabase project endpoint.
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    // The Supabase anon key is read from the environment variable and is used 
+    // to authenticate client-side requests.
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+// Export a singleton instance for direct use
+export const supabase = createBrowserSupabase();
